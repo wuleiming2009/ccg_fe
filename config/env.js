92c977360feb2df5af47e356830d72739a843e5b
wuleiming <369972511@qq.com>
2026-02-12 09:33:01 +0800

    test服

--- config/env.js
+++ config/env.js
@@ -1,6 +1,8 @@
-const mode = "dev";
+const mode = "test";
 const baseUrl = mode === 'dev'
   ? 'http://ccgapi.x-four.cn:8888'
+  : mode === 'test'
+  ? 'https://ccgapi-test.x-four.cn'
   : 'https://ccgapi.x-four.cn';
 
 module.exports = { mode, baseUrl };
